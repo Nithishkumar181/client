90340fdbf9438cd0eaef1f993cbb892aa5d3ad11
Nandhakumar <143866475+nandha2804@users.noreply.github.com>
2025-05-29 09:01:34 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const NavbarEx = () => {
   const navigate = useNavigate();
@@ -28,39 +28,39 @@ const NavbarEx = () => {
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ms-auto">
             <NavDropdown title="Login" id="login-dropdown" className="nav-link-custom">
-              <NavDropdown.Item href="login">Login</NavDropdown.Item>
-              <NavDropdown.Item href="Register">Register</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/login">Login</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/register">Register</NavDropdown.Item>
             </NavDropdown>
           
             <NavDropdown title="Home" id="home-dropdown" className="nav-link-custom">
-              <NavDropdown.Item href="home">Home</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/home">Home</NavDropdown.Item>
             </NavDropdown>
             
             <NavDropdown title="Register" id="register-dropdown" className="nav-link-custom">
-              <NavDropdown.Item href="Register">Register</NavDropdown.Item>
-              <NavDropdown.Item href="ListCustomers">ListCustomers</NavDropdown.Item>
-              <NavDropdown.Item href="UpdateCustomer">UpdateCustomer</NavDropdown.Item>
-              <NavDropdown.Item href="DeleteCustomer">DeleteCustomer</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/register">Register</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/listcustomers">ListCustomers</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/updatecustomer">UpdateCustomer</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/deletecustomer">DeleteCustomer</NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="BookedRooms" id="BookedRooms-dropdown" className="nav-link-custom">
-              <NavDropdown.Item href="BookedRooms">BookedRooms</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/bookedrooms">BookedRooms</NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Facilities" id="facilities-dropdown" className="nav-link-custom">
-              <NavDropdown.Item href="facilities">Facilities</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/facilities">Facilities</NavDropdown.Item>
             </NavDropdown>
 
              <NavDropdown title="Cart" id="Cart-dropdown" className="nav-link-custom">
-              <NavDropdown.Item href="Cart">Cart</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/cart">Cart</NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Services" id="services-dropdown" className="nav-link-custom">
-              <NavDropdown.Item href="services">Services</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/services">Services</NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="About" id="about-dropdown" className="nav-link-custom">
-              <NavDropdown.Item href="about">About</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/about">About</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
